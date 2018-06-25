ab7daaeb6dc06da505a3a846e21c93e8e43fc920
LeightonJonker <leighton.jonker@gmail.com>
2018-06-25 18:52:05 +1200

    button working; added timeouts
    timeouts are added in order to let the functions run and save the data before moving on.

--- PicTexApp/src/app/body/body.component.ts
+++ PicTexApp/src/app/body/body.component.ts
@@ -13,7 +13,7 @@ export class BodyComponent implements OnInit {
   private  baseurl: string = "https://api.flickr.com/services/rest/";
   private APIkey: string = "0109b289e8e411efba6806edf42383e3";
   private secret: string = "e418eb25616d04f4";
-  searchterm: string = "grass";
+  searchterm: string = "cat";
   searchextension: string = "?method=flickr.photos.search&api_key=";
   sizeextension: string = "?method=flickr.photos.getSizes&api_key=";
   private imagesearchurl: string = this.baseurl+this.searchextension+this.APIkey+"&tags="+this.searchterm;
@@ -21,7 +21,7 @@ export class BodyComponent implements OnInit {
 
   private flickrsearch(){ // use flickr apis to search for search term. includes xml creation and usage
 
-    this.role = (<HTMLInputElement>document.getElementById("thetextarea")).value;
+    this.role = (<HTMLInputElement>document.getElementById("thetextarea")).value; //inputted text from textarea
     sessionStorage.setItem('inputtext',this.role );
     var results;
 
@@ -69,9 +69,68 @@ export class BodyComponent implements OnInit {
 
   }
 
-  private buffer(){
-    this.flickrsearch();
-    this.flickrsizes();
+  private flickrandmove(){
+    document.getElementById("submitbutton").innerHTML = "Loading..."
+    this.flickrsearch(); // use search term to search for images
+    setTimeout(() => { // use flickr size finding api to find photos sizes and store them
+
+      var finalsizeurl = this.imagesizeurl
+      var results;
+      var xhr = new Array();
+
+      for (var i=0; i<= 4; i++) {
+        (function(i){
+
+
+          var id = sessionStorage.getItem("id"+i);
+          var finalsizeurl2 = finalsizeurl + "&photo_id="+ id;
+          console.log(finalsizeurl2)
+
+          xhr[i] = new XMLHttpRequest();
+          xhr[i].open('GET', finalsizeurl2, true);
+
+
+          xhr[i].onreadystatechange = function processRequest() {
+            if (this.readyState == 4 && this.status == 200) {
+              document.getElementById("demo").innerHTML = this.responseText;
+              results = this.responseText;
+
+              var parser = new DOMParser();
+              var xmlDoc= parser.parseFromString(results,  "text/xml");
+              var x = xmlDoc.documentElement.getElementsByTagName("size");
+
+              for (var j=0; j<=2; j++ ) {
+                var label = x[j+3].getAttribute("label");
+                var width = x[j+3].getAttribute("width");
+                var height = x[j+3].getAttribute("height");
+                var source = x[j+3].getAttribute("source");
+
+                sessionStorage.setItem(i+ " label "+j, label);
+                sessionStorage.setItem(i+ " width "+j, width);
+                sessionStorage.setItem(i+ " height "+j, height);
+                sessionStorage.setItem(i+ " source "+j, source);
+
+              }
+
+              console.log("got here at least. "+i)
+
+            }
+          };
+
+          xhr[i].send();
+
+
+
+        })(i);
+      }
+
+
+    },1000);
+    setTimeout(() => {window.location.replace("/result")},2500);
+  }
+
+  public printsomething(){
+    console.log("hi after 5")
   }
 
   private flickrsizes(){ // get photo ID; get sizes, save small and small 320 in sessionstorage.
@@ -81,17 +140,20 @@ export class BodyComponent implements OnInit {
 
     for (var i=0; i<= 4; i++) {
       (function(i){
+
+
       var id = sessionStorage.getItem("id"+i);
       var finalsizeurl2 = finalsizeurl + "&photo_id="+ id;
       console.log(finalsizeurl2)
 
         xhr[i] = new XMLHttpRequest();
         xhr[i].open('GET', finalsizeurl2, true);
+
+
         xhr[i].onreadystatechange = function processRequest() {
           if (this.readyState == 4 && this.status == 200) {
             document.getElementById("demo").innerHTML = this.responseText;
             results = this.responseText;
-            console.log("aaaaaaaaaaaaaaaaa")
 
             var parser = new DOMParser();
             var xmlDoc= parser.parseFromString(results,  "text/xml");
@@ -116,6 +178,9 @@ export class BodyComponent implements OnInit {
         };
 
       xhr[i].send();
+
+
+
       })(i);
     }
 
